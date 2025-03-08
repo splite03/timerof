5f6acfc244c2dad5a49811194e912f96163b480a
zaynullin.d <zaynullin.d@promail366.com>
2025-03-08 17:16:36 +0500

    dp

--- src/components/header/Header.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <header class="header">
-    <h1 class="header__name">Timerof</h1>
-  </header>
-</template>
-
-<style lang="scss" scoped>
-    @import "./styles/Header/component";
-</style>
--- src/types/Doors.ts
+++
@@ -1,7 +0,0 @@
-export type Door = {
-    start: number;
-    end: number | undefined;
-};
-export type AllDoors = Record<string | number, {doors: Doors}>;
-
-export type Doors = Door[];
